0955a7ca0571a516ab83eeb16b094431eb671382
Connor Watson <connorwatson@Tammis-MacBook-Pro.local>
2025-12-24 11:13:18 -0600

    C: Website Deployment

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ from textnode import *
 
 import os
 
+import sys
+
 def extract_title(markdown):
     blocks = markdown_to_blocks(markdown)
     for block in blocks:
@@ -27,6 +29,8 @@ def generate_page(source_path, template_path, destination_path):
     title = extract_title(markdown)
     page = template_string.replace("{{ Title }}", title)
     page = page.replace("{{ Content }}", html)
+    page = page.replace('href="/', 'href="{basepath}')
+    page = page.replace('src="/', 'scr="{basepath}')
     dirpath = os.path.dirname(destination_path)
     if dirpath != "":
         os.makedirs(dirpath, exist_ok=True)
@@ -45,6 +49,9 @@ def generate_pages_recusive(source_path, template_path, destination_path):
                 generate_page(source_file, template_path, destination_file)
 
 
+basepath = sys.argv
+
+
 
 
 
@@ -53,9 +60,9 @@ def generate_pages_recusive(source_path, template_path, destination_path):
 def main():
     template_path = "./template.html"
 
-    clean_destination("static", "public")
+    clean_destination("static", "docs")
 
-    generate_pages_recusive("content", template_path, "public")
+    generate_pages_recusive("content", template_path, "docs")
 
 if __name__ == "__main__":
     main()
